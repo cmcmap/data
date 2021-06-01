f6433ae147e9e3d869d3d8130fe3c8dccbe06c73
smal <smaldragon@github.com>
2021-06-01 01:29:55 +0100

    added nation area scripts

---
+++ scripts/nation_area/helpers.py
@@ -0,0 +1,7 @@
+# Acronym table for several civclassic nations, to be updated as needed
+ACRONYMS = {"CCCP": "Communist Cities Caretaker-Protectorate",
+            "JCC": "Joint Coalition Control"}
+
+
+FLAG_REDIRECTS = {"New Sovia":"Sovia",
+                  "Joint DUMP Control":"DUMP"}
